6be89619e7a6ddf00f416246c21880f166a89439
JehuDidimos <jehudidimos.cs@gmail.com>
2024-01-01 21:18:42 -0600

    Init Backend

---
+++ AI-Quizzer-Backend/AI-Quizzer-Backend/src/test/java/com/example/AIQuizzerBackend/AiQuizzerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AIQuizzerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiQuizzerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
